40270439adc55b6d6e81f89286afa40732ec0f7b
Oleg Nykytenko <oleg.nykytenko@gmail.com>
2017-09-27 10:10:17 +0300

    FileAppenderActivity exception processing added.

--- source/onyx/core/appender.d
+++ source/onyx/core/appender.d
@@ -29,23 +29,23 @@ import onyx.bundle;
  * Use by Logger for create new Appender
  *
  * ====================================================================================
- */ 
+ */
 interface AppenderFactory
 {
-	Appender factory(immutable Bundle bundle);
+    Appender factory(immutable Bundle bundle);
 }
 
 
 
 /**
  * Accept messages and publicate it in target
- */ 
+ */
 abstract class Appender
 {
-	/**
- 	 * Append new message
-   	 */ 
-	void append(immutable string message);
+    /**
+     * Append new message
+     */
+    void append(immutable string message);
 }
 
 
@@ -53,24 +53,24 @@ abstract class Appender
  * Factory for NullAppender
  *
  * ====================================================================================
- */ 
+ */
 class NullAppenderFactory:AppenderFactory
 {
-	override Appender factory(immutable Bundle bundle)
-	{
-		return new NullAppender();
-	}
+    override Appender factory(immutable Bundle bundle)
+    {
+        return new NullAppender();
+    }
 }
 
 /**
  * Only Accept messages
- */ 
+ */
 class NullAppender:Appender
 {
-	/**
- 	 * Append new message and do nothing
-   	 */
-	override void append(immutable string message) nothrow pure {}
+    /**
+     * Append new message and do nothing
+     */
+    override void append(immutable string message) nothrow pure {}
 }
 
 
@@ -81,10 +81,10 @@ class NullAppender:Appender
  */
 class ConsoleAppenderFactory:AppenderFactory
 {
-	override Appender factory(immutable Bundle bundle)
-	{
-		return new ConsoleAppender();
-	}
+    override Appender factory(immutable Bundle bundle)
+    {
+        return new ConsoleAppender();
+    }
 }
 
 
@@ -93,15 +93,15 @@ class ConsoleAppenderFactory:AppenderFactory
  */
 class ConsoleAppender:Appender
 {
-	/**
- 	 * Append new message and print it to console
-   	 */
-	@trusted /* writefln is system */
-	override void append(immutable string message)
-	{
-		import std.stdio;
-		writeln(message);
-	}
+    /**
+     * Append new message and print it to console
+     */
+    @trusted /* writefln is system */
+    override void append(immutable string message)
+    {
+        import std.stdio;
+        writeln(message);
+    }
 }
 
 
@@ -112,10 +112,10 @@ class ConsoleAppender:Appender
  */
 class FileAppenderFactory:AppenderFactory
 {
-	override Appender factory(immutable Bundle bundle)
-	{
-		return new FileAppender(bundle);
-	}
+    override Appender factory(immutable Bundle bundle)
+    {
+        return new FileAppender(bundle);
+    }
 }
 
 
@@ -124,32 +124,32 @@ class FileAppenderFactory:AppenderFactory
  */
 class FileAppender:Appender
 {
-	import std.concurrency;
-	
-	/**
-	 * Tid for appender activity
-	 */
-	Tid activity;
-	
-	
-	/**
-	 * Create Appender
-	 */
-	@trusted
-	this(immutable Bundle bundle)
-	{
-		activity = spawn(&fileAppenderActivityStart, bundle);
-	}
-	
+    import std.concurrency;
 
-	/**
- 	 * Append new message and send it to file
-   	 */
-	@trusted
-	override void append(immutable string message)
-	{
-		activity.send(message);
-	}
+    /**
+     * Tid for appender activity
+     */
+    Tid activity;
+
+
+    /**
+     * Create Appender
+     */
+    @trusted
+    this(immutable Bundle bundle)
+    {
+        activity = spawn(&fileAppenderActivityStart, bundle);
+    }
+
+
+    /**
+     * Append new message and send it to file
+     */
+    @trusted
+    override void append(immutable string message)
+    {
+        activity.send(message);
+    }
 }
 
 
@@ -157,9 +157,21 @@ class FileAppender:Appender
  * Start new thread for file log activity
  */
 @system
-void fileAppenderActivityStart(immutable Bundle bundle)
+void fileAppenderActivityStart(immutable Bundle bundle) nothrow
 {
-	new FileAppenderActivity(bundle).run();
+    try
+    {
+        new FileAppenderActivity(bundle).run();
+    }
+    catch (Exception e)
+    {
+        try
+        {
+            import std.stdio;
+            writeln("FileAppenderActivity exception: " ~ e.msg);
+        }
+        catch (Exception ioe){}
+    }
 }
 
 
@@ -171,18 +183,18 @@ void fileAppenderActivityStart(immutable Bundle bundle)
  */
 class FileAppenderActivity
 {
-	import  onyx.core.controller;
-	import std.concurrency;
-	import std.datetime;
-	
-	
-	/**
-	 * Max flush period to write to file
-	 */
-	enum logFileWriteFlushPeriod = 100; // ms
-	
-	
-	/**
+    import  onyx.core.controller;
+    import std.concurrency;
+    import std.datetime;
+
+
+    /**
+     * Max flush period to write to file
+     */
+    enum logFileWriteFlushPeriod = 100; // ms
+
+
+    /**
      * Activity working status
      */
     enum AppenderWorkStatus {WORKING, STOPPING}
@@ -190,83 +202,71 @@ class FileAppenderActivity
 
 
     long startFlushTime;
-    
-    
+
+
     /**
-	 * Max flush period to write to file
-	 */
-	Controller controller;
-    
-    
-	/**
-	 * Primary constructor
-	 *
-	 * Save config path and name
-	 */
+     * Max flush period to write to file
+     */
+    Controller controller;
+
+
+    /**
+     * Primary constructor
+     *
+     * Save config path and name
+     */
     this(immutable Bundle bundle)
     {
-    	try
-    	{
-	    	controller = Controller(bundle);
-		}
-		catch (Exception e)
-		{
-			import std.stdio;
-			writeln("FileAppenderActivity exception: " ~ e.msg);
-		}	
-
-		startFlushTime = Clock.currStdTime();
+        controller = Controller(bundle);
+        startFlushTime = Clock.currStdTime();
     }
 
-    import std.stdio;
-    import std.conv;
-    
-    
+
     /**
-	 * Entry point for start module work
-	 */
-	@system
-	void run()
-	{
-		/**
-		 * Timer cycle for flush log file
-		 */
+     * Entry point for start module work
+     */
+    @system
+    void run()
+    {
+        /**
+         * Main activity cycle
+         */
+        while (workStatus == AppenderWorkStatus.WORKING)
+        {
+            try
+            {
+                workCycle();
+            }
+            catch (Exception e)
+            {
+                import std.stdio;
+                writeln("FileAppenderActivity workcycle exception: " ~ e.msg);
+            }
+        }
+    }
 
-		
-		while (workStatus == AppenderWorkStatus.WORKING)
-		{
-			workCycle();
-		}
-	}
 
+    /**
+     * Activity main cycle
+     */
+    @trusted
+    private void workCycle()
+    {
+        receiveTimeout(
+            dur!("msecs")(10),
+            (string msg)
+            {
+                controller.saveMsg(msg);
+            },
+            (OwnerTerminated e){workStatus = AppenderWorkStatus.STOPPING;},
+            (Variant any){}
+        );
 
-
-	
-	
-	/**
-	 * Activity main cycle
-	 */
-	@trusted
-	private void workCycle()
-	{
-		receiveTimeout(
-			dur!("msecs")(10),
-			(string msg)
-			{
-				controller.saveMsg(msg);
-			},
-			(OwnerTerminated e){workStatus = AppenderWorkStatus.STOPPING;},
-			(Variant any){}
-		);
-
-		if (logFileWriteFlushPeriod <= (Clock.currStdTime() - startFlushTime)/(1000*10))
-		{
-			controller.flush;
-			startFlushTime = Clock.currStdTime();
-		}
-	}
-	
-	
-	
+        if (logFileWriteFlushPeriod <= (Clock.currStdTime() - startFlushTime)/(1000*10))
+        {
+            controller.flush;
+            startFlushTime = Clock.currStdTime();
+        }
+    }
 
 }
